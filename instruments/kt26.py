7a4cae464e1155af7d6ff52d1a27f0c8426fb74e
swapnil2me <40165564+swapnil2me@users.noreply.github.com>
2021-08-12 16:08:04 +0530

    KT26 class basic implementation

--- instruments/kt26.py
+++ instruments/kt26.py
@@ -8,6 +8,7 @@ class KT26(Instrument):
 
         Instrument.__init__(self, address=address, name=name)
 
+
     def read_channel_parameter(self, channel, read):
 
         self.channel.write('funC=smu{}.measure.{}()'.format(channel, read))
@@ -15,16 +16,22 @@ class KT26(Instrument):
         value = float(self.channel.read())
         return value
 
+
     def ramp_channel_voltage(self, channel, set_voltage, ramp_steps=200, ramp_pause=0.05, verbose=True):
 
-        outV = self.read_channel_parameter(channel,'v')
-        rampStep = (outV - set_voltage)/float(ramp_steps)
-        if rampStep == 0.0:
+        current_voltage = self.read_channel_parameter(channel, 'v')
+        voltage_step = (current_voltage - set_voltage)/float(ramp_steps)
+        if voltage_step == 0.0:
             print('{0} at {1} {2}'.format(self.name, set_voltage, self.unit))
             return
         if verbose:
             print('Ramping {0} to {1} {2} in {3} steps'.format(self.name, set_voltage, self.unit, ramp_steps))
-        for i in range(1,ramp_steps+1):
-            increment = (outV - i*rampStep) # in Volts
+        for i in range(1, ramp_steps+1):
+            increment = (current_voltage - i*voltage_step) # in Volts
             self.channel.write('smu{}.source.levelv={}'.format(channel, increment))
             time.sleep(ramp_pause)
+
+
+    def ramp_down(self, channel, ramp_steps=200, ramp_pause=0.05, verbose=False):
+
+        self.ramp_channel_voltage(channel,0,ramp_steps,ramp_pause,verbose)
