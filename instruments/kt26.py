6f580705023ac80b6a5618294a97e87ae4b0a50d
swapnil2me <40165564+swapnil2me@users.noreply.github.com>
2021-08-13 13:21:52 +0530

    KT 26 sampling with buffer

--- instruments/kt26.py
+++ instruments/kt26.py
@@ -1,12 +1,39 @@
 from instrument import Instrument
 import time
-
+import numpy as np
 
 class KT26(Instrument):
 
     def __init__(self, address, name='KT26'):
 
         Instrument.__init__(self, address=address, name=name)
+
+
+    def reset(self):
+        self.channel.write('*RST')
+
+
+    def turn_on(self, output_channel):
+        self.channel.write(f'smu{output_channel}.source.output = smu{output_channel}.OUTPUT_ON')
+
+
+    def turn_off(self, output_channel):
+        self.channel.write(f'smu{output_channel}.source.output = smu{output_channel}.OUTPUT_OFF')
+
+
+    def clear_buffer_all(self):
+        self.channel.write('smua.nvbuffer1.clear()')
+        self.channel.write('smua.nvbuffer2.clear()')
+        self.channel.write('smub.nvbuffer1.clear()')
+        self.channel.write('smub.nvbuffer2.clear()')
+
+
+    def sample(self, parameter, count=10):
+        self.channel.write('smua.measure.count = 10 ')
+        self.channel.write('smua.measure.v(smua.nvbuffer1)')
+        self.channel.write('printbuffer(1, smua.nvbuffer1.n, smua.nvbuffer1)')
+        value = np.float_(self.channel.read().split(','))
+        return value
 
 
     def read_channel_parameter(self, channel, read):
